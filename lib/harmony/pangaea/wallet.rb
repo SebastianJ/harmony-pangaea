13d321dc72cfd10f8e32a15354c8b9af4d5b9d31
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-08-30 21:28:52 +0200

    Update wallet command (properly daemonize/run the command in the background)

--- lib/harmony/pangaea/wallet.rb
+++ lib/harmony/pangaea/wallet.rb
@@ -2,17 +2,21 @@ module Harmony
   module Pangaea
     class Wallet
       
-      def self.send(from_address:, from_shard:, to_address:, to_shard:, amount: nil)
+      def self.send(from_address:, from_shard:, to_address:, to_shard:, amount: nil, daemonize: true)
+        raise ArgumentError, "You need to specify the from address" if from_address.to_s.empty?
+        raise ArgumentError, "You need to specify the from shard"   if from_shard.to_s.empty?
+        raise ArgumentError, "You need to specify the to address"   if to_address.to_s.empty?
+        raise ArgumentError, "You need to specify the to shard"     if to_shard.to_s.empty?
+        raise ArgumentError, "You need to specify the amount"       if amount.to_s.empty?
+        
         amount      ||=   0.00000001
         command       =   "#{::Harmony::Pangaea.configuration.wallet_script} transfer --from #{from_address} --shardID #{from_shard} --to #{to_address} --shardID #{to_shard} --amount #{amount} --pass pass:"
+        command       =   daemonize ? "#{command} > /dev/null 2>&1 &" : command
         output        =   nil
         
         puts "[Harmony::Pangaea::Wallet] - #{Time.now}: Running wallet command:\n#{command}" if ::Harmony::Pangaea.configuration.verbose
 
-        # Background the process so we don't wait for the output
         pid           =   Process.spawn(command)
-        
-        #puts "[Harmony::Pangaea::Wallet] - #{Time.now}: Wallet output:\n#{output}" if ::Harmony::Pangaea.configuration.verbose
       end
             
     end
