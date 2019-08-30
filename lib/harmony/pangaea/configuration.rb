d4718c02b2c53a0bf2fff8ca38cc82ff3f3d8dc5
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-08-30 12:39:42 +0200

    Add more config options

--- lib/harmony/pangaea/configuration.rb
+++ lib/harmony/pangaea/configuration.rb
@@ -1,12 +1,28 @@
 module Harmony
   module Pangaea
     class Configuration
-      attr_accessor :host, :verbose, :faraday
+      attr_accessor :address, :scripts, :host, :verbose, :faraday
     
       def initialize
+        # Make sure to configure your address so the app knows who you are
+        self.address      =   nil
+        
+        self.scripts      =   {
+          node:   "/root/node.sh -t",
+          wallet: "/root/wallet.sh -t"
+        }
+        
         self.host         =   "https://harmony.one"
         self.verbose      =   false
         self.faraday      =   {adapter: :net_http}
+      end
+      
+      def node_script
+        self.scripts[:node]
+      end
+      
+      def wallet_script
+        self.scripts[:wallet]
       end
     
     end
