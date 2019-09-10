262eca18fa4fce5fef5314c14fba7d6b69167103
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-09-10 15:53:02 +0200

    Fix parsing missing balances

--- lib/harmony/pangaea/network/balances.rb
+++ lib/harmony/pangaea/network/balances.rb
@@ -14,12 +14,15 @@ module Harmony
               
               nodes.each do |node|
                 node              =   node.symbolize_keys
-                address           =   node[:address]
-                shard             =   node[:shard]&.to_i
-                balance           =   node[:totalBalance]&.to_f
                 
-                data[shard]     ||=   []
-                data[shard]      <<   {address: address, balance: balance}
+                if !node[:address].to_s.empty? && !node[:shard].to_s.empty? && !node[:totalBalance].to_s.empty?
+                  address           =   node[:address]
+                  shard             =   node[:shard]&.to_i
+                  balance           =   node[:totalBalance]&.to_f
+                  
+                  data[shard]     ||=   []
+                  data[shard]      <<   {address: address, balance: balance}
+                end
               end
             end
             
